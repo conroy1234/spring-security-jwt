347f97570f7deb3753dc893e2c2fce275446c143
conroy <conroyaw@gmail.com>
2024-11-20 15:59:05 +0000

    initial commit

---
+++ src/resources/data.sql
@@ -0,0 +1,1 @@
+INSERT INTO _USER(id,firstName,lastName,email,password)values (1,'conroy','white','someemai.@gmail.com','password123');
---
+++ src/resources/schema.sql
@@ -0,0 +1,7 @@
+CREATE TABLE _USER (
+id varchar(25),
+firstName varchar(255),
+lastName varchar(255),
+email varchAr(255),
+password varchAr(255)
+);
